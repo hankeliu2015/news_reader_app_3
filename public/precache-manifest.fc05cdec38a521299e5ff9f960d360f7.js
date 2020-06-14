6c82541ef7d6d3b9adb8fb06a5110b4c1fb7a155
Hank Liu <hanke.liu@gmail.com>
2020-06-13 21:42:19 -0400

    heroku host test

---
+++ public/precache-manifest.fc05cdec38a521299e5ff9f960d360f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ad9750599c3b331edbb5598b400820",
+    "url": "/index.html"
+  },
+  {
+    "revision": "894bd3cf1a38245711f1",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "6a9d5ec2be9d36b5995a",
+    "url": "/static/css/main.6fb171da.chunk.css"
+  },
+  {
+    "revision": "894bd3cf1a38245711f1",
+    "url": "/static/js/2.54245f56.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.54245f56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a9d5ec2be9d36b5995a",
+    "url": "/static/js/main.9a3341f1.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

44b74b6ef7c1b944a5becfc7e30793444f64a11b
Hank Liu <hanke.liu@gmail.com>
2020-06-16 12:17:39 -0400

    update root path in route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root 'welcome#home'
+
+  # devise_scope :user do
+  #   root to: "devise/sessions#new"
+  # end
+
   resources :likes
   resources :stories
   resources :comments
@@ -7,11 +13,6 @@ Rails.application.routes.draw do
   get 'welcome/home'
   get '/app', to: 'welcome#app', as: 'app'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#home'
-
-  # devise_scope :user do
-  #   root to: "devise/sessions#new"
-  # end
 
   namespace :api do
     namespace :v1 do
